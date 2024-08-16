5092773b78146d8aa7a2dccbaa923720bac8b761
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-16 11:22:04 +0000

    Code Generation

--- components/simple_hero_component_1723803749.js
+++ components/simple_hero_component_1723803749.js
@@ -112,6 +112,10 @@ Vue.component("simple_hero_component_1723803749", {
                             <div>
                             <div>
                             <div>
+                                <label for="narraow" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Narraow</label>
+                                <input type="text" id="narraow" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                            </div>
+                            <div>
                             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                         </form>
                     </div>
